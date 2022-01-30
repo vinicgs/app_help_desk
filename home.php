5b0e739d1123963c4a58541b7c4b897f754ffb75
Vinicius Castro <viniciuscastrogs@gmail.com>
2022-01-29 23:17:53 -0300

    adicionado links para navegação das páginas nos botoes

--- home.php
+++ home.php
@@ -1,60 +1,56 @@
-<?php
-  session_start();
-
-  if(!isset($_SESSION['autenticado']) || $_SESSION['autenticado'] != 'SIM'){
-  header('Location: index.php?login=erro2');
-  }
-
-
-  echo $_SESSION['autenticado'];
- 
-?>
+<?php require_once "validador_acesso.php" ?>
 
 <html>
-  <head>
-    <meta charset="utf-8" />
+<head>
+    <meta charset="utf-8"/>
     <title>App Help Desk</title>
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
     <style>
-      .card-home {
-        padding: 30px 0 0 0;
-        width: 100%;
-        margin: 0 auto;
-      }
+        .card-home {
+            padding: 30px 0 0 0;
+            width: 100%;
+            margin: 0 auto;
+        }
     </style>
-  </head>
+</head>
 
-  <body>
 
-    <nav class="navbar navbar-dark bg-dark">
-      <a class="navbar-brand" href="#">
+<body>
+
+<nav class="navbar navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">
         <img src="logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
         App Help Desk
-      </a>
-    </nav>
+    </a>
+</nav>
 
-    <div class="container">    
-      <div class="row">
+<div class="container">
+    <div class="row">
 
         <div class="card-home">
-          <div class="card">
-            <div class="card-header">
-              Menu
-            </div>
-            <div class="card-body">
-              <div class="row">
-                <div class="col-6 d-flex justify-content-center">
-                  <img src="formulario_abrir_chamado.png" width="70" height="70">
+            <div class="card">
+                <div class="card-header">
+                    Menu
                 </div>
-                <div class="col-6 d-flex justify-content-center">
-                  <img src="formulario_consultar_chamado.png" width="70" height="70">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-6 d-flex justify-content-center">
+                            <a href="abrir_chamado.php">
+                                <img src="formulario_abrir_chamado.png" width="70" height="70">
+                            </a>
+                        </div>
+                        <div class="col-6 d-flex justify-content-center">
+                            <a href="consultar_chamado.php">
+                                <img src="formulario_consultar_chamado.png" width="70" height="70">
+                            </a>
+                        </div>
+                    </div>
                 </div>
-              </div>
             </div>
-          </div>
         </div>
     </div>
-  </body>
+</body>
 </html>
--- valida_login.php
+++ valida_login.php
@@ -11,20 +11,20 @@ $usuarios_app = array(
     array('email' => 'user@teste.com.br', 'senha' => 'abcd')
 );
 
-foreach($usuarios_app as $user){
+foreach ($usuarios_app as $user) {
 
 
-    if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']){
+    if ($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']) {
         $usuario_autenticado = true;
     }
 }
 
-    if($usuario_autenticado){
-        echo 'Usuário autenticado.';
-        $_SESSION['autenticado'] = 'SIM';
-    }else{
-        header('Location: index.php?login=erro');
-        $_SESSION['autenticado'] = 'NAO';
-    }
+if ($usuario_autenticado) {
+    echo 'Usuário autenticado.';
+    $_SESSION['autenticado'] = 'SIM';
+    header('Location: home.php');
 
-?>
+} else {
+    header('Location: index.php?login=erro');
+    $_SESSION['autenticado'] = 'NAO';
+}
